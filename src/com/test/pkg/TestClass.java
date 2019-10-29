3bc5cfb8bca45b983f141109e51e8c7b0ed9b25a
apeet <apeet@EC2AMAZ-3G6UTOM.ad.allstate.com>
2019-10-29 08:16:51 -0400

    second user commit

--- src/com/test/pkg/TestClass.java
+++ src/com/test/pkg/TestClass.java
@@ -4,7 +4,8 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//user2
+		System.out.println("Commit check from second user");
 	}
 
 }
