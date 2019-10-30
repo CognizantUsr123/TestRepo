0acd56d6d2e08023468d8bc240a0ec781e498181
apeet <apeet@EC2AMAZ-C5FMED0.ad.allstate.com>
2019-10-30 04:40:55 -0400

    latest changes

--- src/com/test/pkg/TestClass.java
+++ src/com/test/pkg/TestClass.java
@@ -5,10 +5,6 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//user1 changes
-
-		//user2 changes
-
 		System.out.println("Commit check from second user");
 	}
 
