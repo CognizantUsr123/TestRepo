1ab0ceff2921d04d8e71e27381668c82d466ead1
apeet <apeet@EC2AMAZ-9EAS4TN.ad.allstate.com>
2019-11-04 04:51:42 -0500

    Final Commit

--- src/com/test/pkg/TestClass.java
+++ src/com/test/pkg/TestClass.java
@@ -8,11 +8,11 @@ public class TestClass {
 
 		System.out.println("Commit check from first user conflict check  ");
 
-<<<<<<< HEAD
+
 		System.out.println("Commit check from first usser4");
-=======
+
 		System.out.println("Commit check from first usser3");
->>>>>>> branch 'master' of https://github.com/CognizantUsr123/TestRepo.git
+
 	}
 
 }
